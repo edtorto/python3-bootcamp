bdbc1e9913e85ed4d02f63590cecc4dd41a92ade
edtorto <edwardmtsa@gmail.com>
2025-06-13 22:44:37 +0300

    Add: Add new files and rename existing ones 	renamed:    Day2-DataTypes/MBI_calculator.py -> Day2-DataTypes/BMI_calculator.py 	new file:   Day3-Conditional-Statements/BMI_calculator_upgraded.py 	new file:   Day3-Conditional-Statements/main.py 	new file:   Day3-Conditional-Statements/reminder_checher.py 	new file:   Day3-Conditional-Statements/rollercoaster.py

---
+++ Day3-Conditional-Statements/main.py
@@ -0,0 +1,13 @@
+# if/else condition
+        # if condition is True:
+        #     do this
+        # else: 
+        #     do that
+        
+# comparison operators
+# 1. equals too ==
+# 2. not equal to !=
+# 3. greater than >
+# 4. less than <
+# 5. greater or equal too  >=
+# 6. less than or equal too <=
---
+++ Day3-Conditional-Statements/reminder_checher.py
@@ -0,0 +1,9 @@
+#  this program checks if the number from the user is even or odd
+
+number = int(input("Please enter a number?\n"))
+if number % 2 == 0:
+    print(f"{number} is an EVEN number")
+else:
+    print(f"{number} is an ODD number")
+    
+    
