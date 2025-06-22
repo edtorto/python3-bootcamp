933954e77368f2c43fb0805ef09a7f68e5997e78
edtorto <edwardmtsa@gmail.com>
2025-06-22 16:23:36 +0300

    Modify: Modify Day011-Blackjack-capstone/main.py Add: Add Day012-Scoop-and-number-guessing-game/main

---
+++ Day011-Blackjack-capstone/main.py
@@ -0,0 +1,9 @@
+################### The Blackjack House Rules #######################
+## 1. The deck is unlimited in size
+## 2. There are no Jokers
+## 3. The Jack/King/Queen all count as 10
+## 4. The Ace can count as 1 or 11
+## 5. Use the following as the following list as th deck of cards:
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## 6. The cards in the card have equal probability of being drawn
+## 7. Cards are not removed from the deck as they are being drawn
