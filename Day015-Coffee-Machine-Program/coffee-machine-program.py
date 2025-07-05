6fe5db5ccf98fe26ba599f89772557654d5b0ef7
edtorto <edwardmtsa@gmail.com>
2025-07-05 14:41:41 +0300

    Modified coffee-machine-program.py with full functionality

--- Day015-Coffee-Machine-Program/coffee-machine-program.py
+++ Day015-Coffee-Machine-Program/coffee-machine-program.py
@@ -1,42 +1,44 @@
-
-def print_report(user_input, resource_dict):
-
-    keys = resource_dict.keys()
-    values = resource_dict.values()
-    if user_input == 'report':
-        print(f"{keys[0]}: {values[0]}ml")
-        print(f"{keys[1]}: {values[1]}ml")
-        print(f"{keys[2]}: {values[2]}g")
-        print(f"{keys[3]}: ${values[3]}")
+def check_resources(order_ingredients):
+    """Returns True when the order can be made, and False when the resources are insufficient"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry, that's not enough {item}!")
+            return False
+        return True
     return None
 
-
-def check_resources(drink_name):
-    if drink_name in MENU:
-        details = MENU[drink_name]
-        for item, amount in details["ingredients"].items():
-            print(f"{item}: {amount}")
-        print(f"cost: {details['cost']}")
-    else:
-        print("Sorry, that drink is not on the menu.")
-
 def process_coins():
-    print("\n\n")
+    """Returns the total calculated from the inserted coins."""
+    print("Please insert the coins: ")
+    total = int(input("how many quarters: ")) * 0.25
+    total += int(input("how many dimes: ")) * 0.1
+    total += int(input("how many nicks: ")) * 0.05
+    total += int(input("how many pennies: ")) * 0.01
+    return total
 
-def check_transactions():
-    print("\n\n")
+def check_transactions(money_received, drink_cost):
+    """Return True when the payment is accepted, of False when the money is insufficient"""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change}  in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry, that's not enough money. Money refunded.")
+        return False
 
-def make_coffee():
-    print("\n\n")
-
+def make_coffee(drink_name ,order_ingredients):
+    """Deduct the required ingredients from the resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}. Enjoy!")
 
 resources = {'water': 300,
              'milk': 200,
              'coffee': 100,
              }
-
-# usr_input = input("What would you like to do?: ").lower()
-# print_report(user_input=usr_input, resource_dict=resources)
+profit = 0.00
 
 MENU = {
         "espresso": {
@@ -61,39 +63,21 @@ MENU = {
     },
 }
 
+is_on = True
 
-# print(f"{MENU[""]}: {values[0]}ml")
-# Prompt user by asking “ What would you like? (espresso/latte/cappuccino): ”
-user_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
-print(check_resources(user_choice))
-## Check the user’s input to decide what to do next.
-if user_choice == "espresso":
-    print("Enjoy")
-elif user_choice == "latte":
-    print("Ready")
-elif user_choice == "cappuccino":
-    print("Ok")
-else:
-    print("Sorry, Wrong Choice! Choose from the options.")
-## The prompt should show every time action has completed and again to serve the next customer.
-
-#  Turn off the Coffee Machine by entering “ off ” to the prompt.
-
-#  Print report.
-
-# Check resources sufficient?
-## check if there are enough resources to make that drink.
-## It should not continue to make the drink for insufficient resources
-
-# prompt the user to insert coins.
-
-#  Check transaction successful?
-# Check that the user has inserted enough money to purchase the drink they selected.
-## If not enough refund the Money .
-## But if the user has inserted enough money, make coffee and a report
-## If the user has inserted too much money, the machine should offer change, coffee and report.
-
-#  Make Coffee.
-
-## If the transaction is successful and there are enough resources make coffee, offer a report before and after
-## Serve Coffe
+while is_on:
+    # Prompt user by asking “ What would you like? (espresso/latte/cappuccino): ”
+    user_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if user_choice == 'off':
+        is_on = False
+    elif user_choice == 'report':
+        print(f"water: {resources['water']}ml")
+        print(f"milk: {resources['milk']}ml")
+        print(f"coffee': {resources['coffee']}g")
+        print(f"cost: ${profit}")
+    else:
+        drink = MENU[user_choice]
+        if check_resources(drink['ingredients']):
+            payment = process_coins()
+            if check_transactions(payment, drink['cost']):
+                make_coffee(user_choice, drink['ingredients'])
