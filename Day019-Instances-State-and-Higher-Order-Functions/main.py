000120f32a08942b404fc239ed0ddb73cb7a3500
edtorto <edwardmtsa@gmail.com>
2025-07-12 18:27:46 +0300

    Modified Day019-Instances-State-and-Higher-Order-Functions/main.py with drawing functions

--- Day019-Instances-State-and-Higher-Order-Functions/main.py
+++ Day019-Instances-State-and-Higher-Order-Functions/main.py
@@ -5,6 +5,26 @@ timmy = Turtle()
 def move_forwards():
     timmy.forward(10)
 
+def move_backwards():
+    timmy.backward(10)
+
+def turn_left():
+    timmy.left(10)
+
+def turn_right():
+    timmy.right(10)
+
+def clear_screen():
+    timmy.clear()
+    timmy.penup()
+    timmy.home()
+    timmy.pendown()
+
+screen.textinput("Make your bet", "Who will win? Enter a color:")
 screen.listen()
-screen.onkey( move_forwards, "space")
+screen.onkey( move_forwards, "w")
+screen.onkey( move_backwards, "s")
+screen.onkey( turn_left, "a")
+screen.onkey( turn_right, "d")
+screen.onkey( clear_screen, "c")
 screen.exitonclick()
