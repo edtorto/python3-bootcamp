90e812b6f68a71c69076c2c6107a4e5f7cceb2a0
edtorto <edwardmtsa@gmail.com>
2025-07-15 11:26:31 +0300

    Modified  Day020-animation-and-coordinates-snake-game1/screen-setup.py with turtle settings

--- Day020-animation-and-coordinates-snake-game1/screen-setup.py
+++ Day020-animation-and-coordinates-snake-game1/screen-setup.py
@@ -5,10 +5,20 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Snake Game")
 
-turtle = Turtle()
-turtle.shape("square")
-turtle.color("white")
+turtle1 = Turtle()
+turtle1.shape("square")
 
+turtle1.color("white")
+turtle1.goto(x=0, y=0)
 
+turtle2 = Turtle()
+turtle2.shape("square")
+turtle2.color("white")
+turtle2.goto(x=20, y=0)
+
+turtle3 = Turtle()
+turtle3.shape("square")
+turtle3.color("white")
+turtle3.goto(x=40,y=0)
 
 screen.exitonclick()
