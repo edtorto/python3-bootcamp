43dc840d43c6d2274c61815e036ca6a19aed8065
edtorto <edwardmtsa@gmail.com>
2025-08-02 14:54:20 +0300

    Modified Day025 Day025-working-with-csv-and-pandas/us-sates-game/main.py with list comprehension

--- Day025-working-with-csv-and-pandas/us-sates-game/main.py
+++ Day025-working-with-csv-and-pandas/us-sates-game/main.py
@@ -20,12 +20,16 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title= f"{len(guessed_states)}/50 States Correct",
                                     prompt="What is another states's name?").title()
     if answer_state == "Exit":
-        missed_states = []
-        for state in states_data:
-            if state not in guessed_states:
-                missed_states.append(state)
-        new_data = pd.DataFrame(missed_states)
-        new_data.to_csv("states_to_learn.csv")
+        # missed_states = []
+        # for state in states_data:
+        #     if state not in guessed_states:
+        #         missed_states.append(state)
+        #
+    #Modified with list comprehension
+        missed_states = [state for state in states_data
+                         if state not in guessed_states]
+
+        pd.DataFrame(missed_states).to_csv("states_to_learn.csv")
     if answer_state in states_data:
         guessed_states.append(answer_state)
         state_data = data[data.state == answer_state]
