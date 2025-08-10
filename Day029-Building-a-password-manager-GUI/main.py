7938f70c38f283ed88f06072b5d789e8e53b0fc1
edtorto <edwardmtsa@gmail.com>
2025-08-10 22:24:40 +0300

    Added data.txt file Modified main.py with the missing functions

--- Day029-Building-a-password-manager-GUI/main.py
+++ Day029-Building-a-password-manager-GUI/main.py
@@ -1,10 +1,55 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 FONT_NAME = "sans-serif"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def pass_generator():
+    """Generates a password of given length"""
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+               'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+               'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    symbols = [ '!', '#', '$', '%', '&', '(', ')', '*', '+']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    #comprehension lists
+    letter_list = [choice(letters) for char in range(randint(8,10))]
+    symbol_list = [choice(symbols) for symbol in range(randint(2,4))]
+    number_list = [choice(numbers) for num in range(randint(2,4))]
+
+    password_list = letter_list + symbol_list + number_list
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.insert(0,password)
+
+    pyperclip.copy(password)#copies the password to clipboard
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_data():
+    website = web_entry.get().title()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(email) < 16 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title="Save?", message=f"These are the details entered:\n"
+                                                  f"Website: {website}\n Email: {email}\n "
+                                                  f"Password: {password}\n Is it Ok to save?")
+        if is_ok:
+            file_link = "data.txt"
+            with open(file_link, "a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+                messagebox.showinfo(title="Saved!", message=f"Saved successfully! to {file_link}")
+                web_entry.delete(0, END)
+                password_entry.delete(0, END)
+                web_entry.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -31,15 +76,17 @@ password_label.grid(column=0, row=3)
 #entries
 web_entry = Entry(width=60)
 web_entry.grid(column=1, row=1, columnspan=2)
+web_entry.focus()
 email_entry = Entry(width=60)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "e_name@gmail.com")
 password_entry = Entry(width=34)
 password_entry.grid(column=1, row=3)
 
 #buttons
-generate_btn = Button(text="Generate Password", font=FONT_NAME, bg="white", )
+generate_btn = Button(text="Generate Password", font=FONT_NAME, bg="white",command=pass_generator )
 generate_btn.grid(column=2, row=3)
-add_btn = Button(text="Add", font=FONT_NAME, width=39, bg="white")
+add_btn = Button(text="Add", font=FONT_NAME, width=39, bg="white", command=save_data)
 add_btn.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
